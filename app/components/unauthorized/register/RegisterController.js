3393e65addb484248ff42ec2e284079367616079
George Margaritis <geormarga@gmail.com>
2017-10-25 20:54:05 +0300

    Add validation logic to Register component and md-dialog as well to display error message.

--- app/components/unauthorized/register/RegisterController.js
+++ app/components/unauthorized/register/RegisterController.js
@@ -1,11 +1,11 @@
 (function () {
     'use strict';
 
-    angular     
+    angular
         .module('TBA')
         .controller('RegisterController', RegisterController);
 
-    function RegisterController() {
+    function RegisterController($mdDialog) {
 
         var vm = this;
 
@@ -14,16 +14,36 @@
         vm.email = "";
         vm.password = "";
         vm.retypePassword = "";
-        
-        function validate(){
-            vm.email = "";
+
+        function validate() {
+            console.log(vm.password);
+            console.log(vm.retypePassword);
+            console.log(vm.password === vm.retypePassword);
             return vm.password === vm.retypePassword;
         }
-        
-        function register() {
+
+        function register(ev) {
+            if (!validate()) {
+                modalDialog(ev, "Passwords do not match!");
+            }
             vm.username = "";
             vm.email = "";
             vm.password = "";
             vm.retypePassword = "";
-        }}
+        }
+
+        function modalDialog(ev, text) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            // Modal dialogs should fully cover application
+            // to prevent interaction outside of dialog
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#main-view')))
+                    .clickOutsideToClose(true)
+                    .textContent(text)
+                    .ok('Okay')
+                    .targetEvent(ev)
+            );
+        }
+    }
 })();
