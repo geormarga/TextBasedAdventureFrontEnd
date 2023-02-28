084303641f3938d71b34bfbe872eaa18b0ffccc6
George Margaritis <georgios.margaritis@klarna.com>
2023-02-28 19:26:16 +0100

    Provide some logic in the save controller

--- app/components/authorized/save/SaveController.js
+++ app/components/authorized/save/SaveController.js
@@ -5,16 +5,23 @@
         .module('TBA')
         .controller('SaveController', SaveController);
 
-    function SaveController(alertFactory) {
+    function SaveController(savedService, $window, $state, alertFactory) {
 
         var vm = this;
 
         vm.save = save;
+        vm.username = $window.sessionStorage.user;
 
         function save(ev) {
             //saves and on success displays popup
-            //alert('Game saved successfully');
-            alertFactory.alert(ev, "Game saved successfully.");
+            loadService.save(vm.username).then(function (success) {
+                // Create cookie at this point and keep it.
+                alertFactory.alert(ev, "Game saved successfully.");
+                $state.go('authorized.play');
+                return success;
+            }).catch(function (error) {
+                alertFactory.alert(ev, "Something went wrong while trying to save the game!");
+            });
         }
     }
 })();
