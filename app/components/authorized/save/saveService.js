3a231204cbc1e22e072baca5c4360333be2565a1
George Margaritis <georgios.margaritis@klarna.com>
2023-02-28 19:25:42 +0100

    Use mock data for the save service for now

--- app/components/authorized/save/saveService.js
+++ app/components/authorized/save/saveService.js
@@ -1,1 +1,35 @@
 //Update request with username to save current state for username in persistence
+angular
+    .module('TBA')
+    .service('saveService', saveService);
+
+function saveService(Restangular) {
+
+    let service = {
+        save: save
+    };
+    return service;
+
+    function save(username) {
+        let serviceCall = tryToSaveTheGame(username);
+        return serviceCall.then(function (result) {
+            return result;
+        });
+    }
+
+    // Actual service call to be replaced here
+    function tryToSaveTheGame(username) {
+        if (username === "talos") {
+            username = "1";
+        }
+        let postCall = Restangular.oneUrl('posts', 'https://jsonplaceholder.typicode.com/posts/' + username + '').get();
+        return postCall.then(
+            function (object) {
+                return object;
+            },
+            function (result) {
+                console.log("Something went wrong", result.status);
+                throw result.status;
+            });
+    }
+}
